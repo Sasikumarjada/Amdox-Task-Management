f8bc39f658c1d3b3e82e8ad9e0dc9d2117cbdc35
Sasikumar Jada <sasikumarjada2004@gmail.com>
2025-11-01 20:26:45 +0530

    Create NotificationRepository.java

---
+++ src/main/java/com/amdox/taskmanagement/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.amdox.taskmanagement.repository;
+
+import com.amdox.taskmanagement.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
+    long countByUserIdAndIsReadFalse(Long userId);
+}
