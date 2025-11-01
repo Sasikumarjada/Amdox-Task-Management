48c037c514c9f39043cf8706f38dfaccd18d2c45
Sasikumar Jada <sasikumarjada2004@gmail.com>
2025-11-01 20:30:11 +0530

    Create AuthResponse.java

---
+++ src/main/java/com/amdox/taskmanagement/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.amdox.taskmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private String fullName;
+    private String role;
+}
